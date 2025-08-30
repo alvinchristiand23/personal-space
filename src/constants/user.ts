e64b74227c0cf0f4a4cf63b8efee0c207ca6b9e4
alvinchristiand23 <alvinchristiand23@gmail.com>
2025-08-30 23:40:11 +0800

    chore(constants): add user constants

---
+++ src/constants/user.ts
@@ -0,0 +1,9 @@
+export const NAME = "Alvin Christian D";
+export const USERNAME = "@alvinchristiand23";
+export const CONTACT = {
+  whatsapp: "https://wa.me/+6282290491884",
+  mail: "mailto:alvinchristiand23@gmail.com",
+  linkedin: "https://linkedin.com/in/alvinchristiand23",
+  instagram: "https://instagram.com/alvinchristiand23",
+  github: "https://github.com/alvinchristiand23",
+};
